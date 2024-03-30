7ff524860838995db40a56190b2ba6d785aa78bc
sasaova123 <165455334+sasaova123@users.noreply.github.com>
2024-03-30 11:46:18 +0500

    краноярс
    моя игра твоя игра

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import pygame as pg 
 
-    class Game:
-        def __init__(self) -> object:
-            pass
+pg.init()
+class Game:
+    def __init__(self) -> object:
+        window_size: tuple =(300,300)
+        pg.display.set_caption("моя играть твоя игра он такая как и твоя")
+        self.screen = pg.display.set_mode(window_size)
+        self.screen.fill((0, 0, 255))
+
+        pg.display.flip()
+            
+
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    exit()
+
+a = Game()
